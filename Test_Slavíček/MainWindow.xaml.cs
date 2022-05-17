f4b4a444f33de694aa7f4509dbb09d08ea8fce24
slavicek.martin <slaviekmartin@post.cz>
2022-05-17 11:39:51 +0200

    Přidejte soubory projektu.

---
+++ Test_Slavíček/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test_Slavíček
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Nakladak Tatra;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Tatra = new Nakladak();
+            Tatra.Jmeno = "Tatra";
+            Zobraz(Tatra, txtbox1);
+
+            
+        }
+        public void Zobraz(Nakladak nakladak, TextBox textBox)
+        {
+            textBox.Text = "Jmeno: " + nakladak.Jmeno.ToString() + "\n";
+            textBox.Text += "Spotřeba: " + nakladak.Spotreba.ToString() + "\n";
+            textBox.Text += "Stav Tachometru: " + nakladak.Stav_Tachometru.ToString() + ("\n");
+            textBox.Text += "Naklad: " + nakladak.Naklad.ToString() + "\n";
+            textBox.Text += "Stav Paliva: " + nakladak.Stav_Paliva.ToString() + ("\n");
+            textBox.Text += "Nosnost: " + nakladak.Nosnost.ToString() + "\n";
+
+
+        }
+
+        private void btn_Tankovat_Click(object sender, RoutedEventArgs e)
+        {
+            Tatra.Natanokovat();
+            Zobraz (Tatra, txtbox1);
+        }
+
+        private void btn_Naložit_Click(object sender, RoutedEventArgs e)
+        {
+            Tatra.Nalozit();
+            Zobraz (Tatra, txtbox1);
+        }
+
+        private void btn_Jet_Click(object sender, RoutedEventArgs e)
+        {
+            Tatra.Jet();
+            Zobraz(Tatra, txtbox1);
+        }
+    }
+
+}
---
+++ Test_Slavíček/Nakladak.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Slavíček
+{
+    public class Nakladak
+    {
+        private int naklad;
+        private int stav_paliva;
+        private int stav_Tachometru;
+        private int nosnost;
+
+
+        public string Jmeno { get; set; }        
+        public int Spotreba { get; set; }
+        public int Naklad { get => naklad; }
+        public int Stav_Paliva { get => stav_paliva; }
+        public int Stav_Tachometru { get => stav_Tachometru;}
+        public int Nosnost { get => nosnost; }
+       
+        public Nakladak()
+        {
+            Jmeno = " ";
+            Spotreba = 5 ;
+            naklad = 0;
+            stav_paliva = 30;
+            stav_Tachometru = 0;
+            nosnost = 3000;
+            
+
+        }
+        public void Natanokovat()
+        {
+            stav_paliva += 5;
+            if (stav_paliva > 50)
+            {
+                stav_paliva = 50;
+            }
+
+        }
+        public void Nalozit()
+        {
+            naklad += 100;
+            if (naklad > 3000)
+            {
+                naklad = 3000;
+            }
+        }
+        public void Jet()
+        {
+            stav_paliva -= 5;
+            if (stav_paliva < 0)
+            {
+                stav_paliva = 0;
+            }
+            else
+            {
+                stav_Tachometru += 50;
+               if (stav_Tachometru < 100)
+                {
+                    stav_Tachometru = 100;
+                }
+            }
+           
+            
+        }
+    }
+}
